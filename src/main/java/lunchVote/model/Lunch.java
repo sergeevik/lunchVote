6125ef317d40bfdc95d84ff2434b6784ab9200da
Viktor <vampirit@mail.ru>
2017-12-20 23:56:43 +0400

    get set constructors

--- src/main/java/lunchVote/model/Lunch.java
+++ src/main/java/lunchVote/model/Lunch.java
@@ -4,7 +4,64 @@ import java.time.LocalDate;
 
 public class Lunch extends AbstractBaseEntity{
     private LocalDate date;
-    private String Description;
+    private String description;
     private Integer price;
     private Restaurant restaurant;
+
+    public Lunch(Integer id, LocalDate date, String description, Integer price, Restaurant restaurant) {
+        super(id);
+        this.date = date;
+        this.description = description;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
+
+    public Lunch(Lunch lunch) {
+        this(lunch.id, lunch.date, lunch.description, lunch.price, lunch.restaurant);
+    }
+
+    public Lunch() {
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public String toString() {
+        return "Lunch{" +
+                "date=" + date +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", restaurant=" + restaurant +
+                '}';
+    }
 }
