1f81ca4dbe0653836ed81ed847acc74773b5b343
Viktor <vampirit@mail.ru>
2017-12-20 23:57:43 +0400

    in memory realize lunch repository

---
+++ src/main/java/lunchVote/repository/inMemoryRepository/InMemoryLunchRepository.java
@@ -0,0 +1,52 @@
+package lunchVote.repository.inMemoryRepository;
+
+import lunchVote.model.Lunch;
+import lunchVote.repository.LunchRepository;
+import lunchVote.repository.testData.LunchData;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class InMemoryLunchRepository implements LunchRepository {
+    private static final Map<Integer, Lunch> repo = new HashMap<>();
+    private static AtomicInteger counter;
+
+    public static void initRepo() {
+        repo.clear();
+        repo.put(1, LunchData.LUNCH1);
+        repo.put(2, LunchData.LUNCH2);
+        repo.put(3, LunchData.LUNCH3);
+        counter = new AtomicInteger(repo.size());
+    }
+
+    @Override
+    public List<Lunch> getAllForDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public Lunch save(Lunch object) {
+        object.setId(1);
+        return object;
+    }
+
+    @Override
+    public Lunch getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Lunch> getAll() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+}
