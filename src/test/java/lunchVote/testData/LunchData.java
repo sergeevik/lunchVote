dc8b790be37eb0b7ee08cb700916b5098b8c5d5a
Viktor <vampirit@mail.ru>
2017-12-21 20:56:22 +0400

    change Id's

--- src/test/java/lunchVote/testData/LunchData.java
+++ src/test/java/lunchVote/testData/LunchData.java
@@ -9,9 +9,9 @@ import java.util.List;
 import static lunchVote.testData.RestaurantData.*;
 
 public class LunchData {
-    public static final Lunch VOPER = new Lunch(1, LocalDate.now(), "Voper", 12000, BURGER_KING);
-    public static final Lunch BIG_MACK = new Lunch(2, LocalDate.now(), "Big mack", 8000, MC_DONALD);
-    public static final Lunch CHIKEN = new Lunch(3, LocalDate.now(), "Chiken", 10000, KFC);
+    public static final Lunch VOPER = new Lunch(100006, LocalDate.now(), "Voper", 12000, BURGER_KING);
+    public static final Lunch BIG_MACK = new Lunch(100007, LocalDate.now(), "Big mack", 8000, MC_DONALD);
+    public static final Lunch CHIKEN = new Lunch(100008, LocalDate.now(), "Chiken", 10000, KFC);
 
     public static List<Lunch> allLunch(){
         return Arrays.asList(VOPER, BIG_MACK, CHIKEN);
