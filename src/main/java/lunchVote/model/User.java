c181c4f095e8c8cd971fdfcde11e4660e5fe8075
Viktor <vampirit@mail.ru>
2017-12-21 18:49:32 +0400

    mapping + update to javaBean(constructors, get\set, equals and hashCode) (User)

--- src/main/java/lunchVote/model/User.java
+++ src/main/java/lunchVote/model/User.java
@@ -1,13 +1,122 @@
 package lunchVote.model;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User extends AbstractBaseEntity{
+
+    @Column(name = "name")
+    @NotNull
     private String name;
+
+    @Column(name = "email")
+    @Email
+    @NotNull
     private String email;
+
+    @Column(name = "password")
+    @Length(min = 5)
     private String password;
-    private LocalDate registered;
-    private boolean enabled;
+
+    @Column(name = "registered")
+    private LocalDate registered = LocalDate.now();
+
+    @Column(name = "enabled")
+    private boolean enabled = true;
+
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
     private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(User user) {
+        this(user.id, user.name, user.email, user.password, user.registered, user.enabled, user.roles);
+    }
+
+    public User(Integer id, @NotNull String name, @Email @NotNull String email, @Length(min = 5) String password, LocalDate registered, boolean enabled, Set<Role> roles) {
+        super(id);
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.registered = registered;
+        this.enabled = enabled;
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(LocalDate registered) {
+        this.registered = registered;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
 }
