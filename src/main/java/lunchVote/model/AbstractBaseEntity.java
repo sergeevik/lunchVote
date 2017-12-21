dfc2cdaa733c0f2f5a4cb6687077f4c1f387d5ab
Viktor <vampirit@mail.ru>
2017-12-21 18:28:14 +0400

    mapping + equals and hashcode (AbstractBaseEntity)

--- src/main/java/lunchVote/model/AbstractBaseEntity.java
+++ src/main/java/lunchVote/model/AbstractBaseEntity.java
@@ -1,6 +1,14 @@
 package lunchVote.model;
 
+import javax.persistence.*;
+
+@MappedSuperclass
 public abstract class AbstractBaseEntity {
+    private static final int SEQ_START = 100000;
+
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", initialValue = SEQ_START, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     protected Integer id;
 
     public AbstractBaseEntity() {
@@ -22,5 +30,18 @@ public abstract class AbstractBaseEntity {
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
 
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
