03d5473bab5bacb3f080f3ed96920b3aa76c9bc9
Viktor <vampirit@mail.ru>
2017-12-21 20:44:07 +0400

    add transactions

--- src/main/java/lunchVote/repository/dataJpa/RestaurantDataJpaRepositoryImpl.java
+++ src/main/java/lunchVote/repository/dataJpa/RestaurantDataJpaRepositoryImpl.java
@@ -5,16 +5,19 @@ import lunchVote.repository.RestaurantRepository;
 import lunchVote.repository.dataJpa.springCrud.RestaurantCrud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class RestaurantDataJpaRepositoryImpl implements RestaurantRepository {
 
     @Autowired
     private RestaurantCrud crud;
 
     @Override
+    @Transactional
     public Restaurant save(Restaurant object) {
         if (!object.isNew() && getById(object.getId()) == null) {
             return null;
@@ -34,6 +37,7 @@ public class RestaurantDataJpaRepositoryImpl implements RestaurantRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return crud.delete(id) != 0;
     }
