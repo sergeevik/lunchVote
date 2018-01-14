9d70158cbd3874a1c738673f9deb3c93f9a0a45f
Viktor <vampirit@mail.ru>
2018-01-15 00:14:12 +0400

    need List counters, not one

--- src/main/java/lunchVote/service/VoteService.java
+++ src/main/java/lunchVote/service/VoteService.java
@@ -4,8 +4,9 @@ import lunchVote.model.Vote;
 import lunchVote.transferObjects.VoteCounter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface VoteService {
     Vote save(int lunchId, int userId, LocalDate date);
-    VoteCounter getDayResult(LocalDate date);
+    List<VoteCounter> getDayResult(LocalDate date);
 }
