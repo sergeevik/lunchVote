efb98807f69f9eea1e1d582f145dc2a47149678b
Viktor <vampirit@mail.ru>
2018-01-15 01:30:21 +0400

    fix tests and test data

--- src/test/java/lunchVote/repository/VoteRepositoryTest.java
+++ src/test/java/lunchVote/repository/VoteRepositoryTest.java
@@ -1,12 +1,12 @@
 package lunchVote.repository;
 
 import lunchVote.model.Vote;
+import lunchVote.repository.dataJpa.springCrud.VoteCrud;
 import lunchVote.transferObjects.VoteCounter;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,80 +14,68 @@ import java.util.List;
 import static lunchVote.testData.LunchData.CHIKEN;
 import static lunchVote.testData.UserData.ADMIN;
 import static lunchVote.testData.UserData.JURA;
-import static lunchVote.testData.VoteData.ADMIN_VOTE;
 import static lunchVote.testData.VoteData.JURA_VOTE;
+import static lunchVote.testData.VoteData.SAVE_VOTE_COUNT_QUERY;
 import static lunchVote.testData.VoteData.USER_VOTE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class VoteRepositoryTest extends SpringConfigOnTests{
 
     @Autowired
-    private VoteRepository repository;
+    private VoteCrud repository;
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void getByUserIdAndDateNull() throws Exception {
-        Vote byUserIdAndDate = repository.getByUserIdAndDate(ADMIN.getId(), LocalDate.now().minusDays(5));
+        Vote byUserIdAndDate = repository.findByUserAndDate(ADMIN.getId(), LocalDate.now().minusDays(5));
         assertThat(byUserIdAndDate).isNull();
     }
 
     @Test
     public void getByUserIdAndDate() throws Exception {
-        Vote byUserIdAndDate = repository.getByUserIdAndDate(JURA.getId(), JURA_VOTE.getDate());
+        Vote byUserIdAndDate = repository.findByUserAndDate(JURA.getId(), JURA_VOTE.getDate());
         assertThat(byUserIdAndDate).isNotNull()
                 .isEqualToComparingFieldByField(JURA_VOTE);
     }
 
     @Test
     public void getByUserIdAndDateFakeUser() throws Exception {
-        Vote byUserIdAndDate = repository.getByUserIdAndDate(41, LocalDate.now().minusDays(5));
+        Vote byUserIdAndDate = repository.findByUserAndDate(41, LocalDate.now().minusDays(5));
         assertThat(byUserIdAndDate).isNull();
     }
 
     @Test
-    public void saveWithFakeUserId() throws Exception {
-        exception.expect(DataIntegrityViolationException.class);
-        repository.save(42, 41, LocalDate.now());
-    }
-
-    @Test
-    public void saveNormal() throws Exception {
-        Vote save = repository.save(USER_VOTE.getLunchId(), USER_VOTE.getUserId(), USER_VOTE.getDate());
+    public void save() throws Exception {
+        Vote save = repository.save(USER_VOTE);
 
         assertThat(save).isNotNull();
 
-        Vote byUserIdAndDate = repository.getByUserIdAndDate(USER_VOTE.getUserId(), USER_VOTE.getDate());
+        Vote byUserIdAndDate = repository.findByUserAndDate(USER_VOTE.getUserId(), USER_VOTE.getDate());
         assertThat(byUserIdAndDate).isEqualToComparingFieldByField(USER_VOTE);
     }
 
     @Test
     public void getLunchVotesEmpty() throws Exception {
-        List<VoteCounter> lunchVotesOnDate = repository.getLunchVotesOnDate(LocalDate.now().minusDays(5));
+        List<VoteCounter> lunchVotesOnDate = repository.getLunchVoteOnDate(LocalDate.now().minusDays(5));
         assertThat(lunchVotesOnDate).isNotNull().isEmpty();
     }
 
     @Test
     public void getLunchVotes() throws Exception {
-        List<VoteCounter> lunchVotesOnDate = repository.getLunchVotesOnDate(LocalDate.now());
+        List<VoteCounter> lunchVotesOnDate = repository.getLunchVoteOnDate(LocalDate.now());
         assertThat(lunchVotesOnDate).hasSize(1);
     }
 
     @Test
     public void getLunchVotesAfterVote() throws Exception {
-        repository.save(USER_VOTE.getLunchId(), USER_VOTE.getUserId(), USER_VOTE.getDate());
+        repository.save(USER_VOTE);
 
-        List<VoteCounter> lunchVotesOnDate = repository.getLunchVotesOnDate(LocalDate.now());
+        List<VoteCounter> lunchVotesOnDate = repository.getLunchVoteOnDate(LocalDate.now());
         assertThat(lunchVotesOnDate).hasSize(2);
     }
 
-    @Test
-    public void updateVote() throws Exception {
-        repository.save(CHIKEN.getId(), ADMIN_VOTE.getUserId(), ADMIN_VOTE.getDate());
-        Vote adminVote = repository.getByUserIdAndDate(ADMIN_VOTE.getUserId(), ADMIN_VOTE.getDate());
-        assertThat(adminVote.getLunchId()).isEqualTo(CHIKEN.getId());
-    }
 
     /*
     ===========================
@@ -99,24 +87,26 @@ public class VoteRepositoryTest extends SpringConfigOnTests{
     @Test
     public void getByUserIdAndDateQueryCount() throws Exception {
         countQueries.setLimit(1);
-        repository.getByUserIdAndDate(JURA.getId(), LocalDate.now());
+        repository.findByUserAndDate(JURA.getId(), LocalDate.now());
     }
 
     @Test
     public void saveQueryCount() throws Exception {
-        countQueries.setLimit(3);
-        repository.save(USER_VOTE.getLunchId(), USER_VOTE.getUserId(), USER_VOTE.getDate());
+        countQueries.setLimit(2);
+        repository.save(SAVE_VOTE_COUNT_QUERY);
     }
 
     @Test
     public void updateQueryCount() throws Exception {
         countQueries.setLimit(2);
-        repository.save(CHIKEN.getId(), ADMIN_VOTE.getUserId(), ADMIN_VOTE.getDate());
+        Vote vote = new Vote(JURA_VOTE);
+        vote.setLunchId(CHIKEN.getId());
+        repository.save(vote);
     }
 
     @Test
     public void getLunchVotesOnDateQueryCount() throws Exception {
         countQueries.setLimit(2);
-        repository.getLunchVotesOnDate(LocalDate.now());
+        repository.getLunchVoteOnDate(LocalDate.now());
     }
 }
--- src/test/java/lunchVote/testData/VoteData.java
+++ src/test/java/lunchVote/testData/VoteData.java
@@ -15,9 +15,11 @@ import static lunchVote.testData.UserData.*;
   (100002, 100008);
  */
 public class VoteData {
+
     public static final Vote ADMIN_VOTE = new Vote(100009, ADMIN.getId(), VOPER.getId(), LocalDate.now());
     public static final Vote JURA_VOTE = new Vote(100010, JURA.getId(), VOPER.getId(), LocalDate.now());
     public static final Vote USER_VOTE = new Vote(100011, USER.getId(), CHIKEN.getId(), LocalDate.now());
+    public static final Vote SAVE_VOTE_COUNT_QUERY = new Vote(null, USER.getId(), CHIKEN.getId(), LocalDate.now().plusDays(3));
 
     public static Iterable<Vote> getAll(){
         return Arrays.asList(ADMIN_VOTE, JURA_VOTE, USER_VOTE);
