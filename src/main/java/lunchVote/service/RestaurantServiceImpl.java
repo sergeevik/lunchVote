8469cccf907b16c8ff284f83f276fadeccac280b
Viktor <vampirit@mail.ru>
2018-01-15 00:08:33 +0400

    refactor logic and rename

--- src/main/java/lunchVote/service/RestaurantServiceImpl.java
+++ src/main/java/lunchVote/service/RestaurantServiceImpl.java
@@ -1,7 +1,6 @@
-package lunchVote.repository.dataJpa;
+package lunchVote.service;
 
 import lunchVote.model.Restaurant;
-import lunchVote.repository.RestaurantRepository;
 import lunchVote.repository.dataJpa.springCrud.RestaurantCrud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,24 +10,23 @@ import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
-public class RestaurantRepositoryImplDataJpa implements RestaurantRepository {
+public class RestaurantServiceImpl implements RestaurantService {
+
+    private final RestaurantCrud crud;
 
     @Autowired
-    private RestaurantCrud crud;
-
-    @Override
-    @Transactional
-    public Restaurant save(Restaurant object) {
-        if (!object.isNew() && getById(object.getId()) == null) {
-            return null;
-        }else {
-            return crud.save(object);
-        }
+    public RestaurantServiceImpl(RestaurantCrud crud) {
+        this.crud = crud;
     }
 
     @Override
-    public Restaurant getById(int id) {
-        return crud.findById(id).orElse(null);
+    @Transactional
+    public Restaurant save(Restaurant restaurant) {
+        if (!restaurant.isNew() && get(restaurant.getId()) == null) {
+            return null;
+        }else {
+            return crud.save(restaurant);
+        }
     }
 
     @Override
@@ -40,5 +38,10 @@ public class RestaurantRepositoryImplDataJpa implements RestaurantRepository {
     @Transactional
     public boolean delete(int id) {
         return crud.delete(id) != 0;
+    }
+
+    @Override
+    public Restaurant get(int id) {
+        return crud.findById(id).orElse(null);
     }
 }
