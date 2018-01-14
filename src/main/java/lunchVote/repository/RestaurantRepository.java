c73e02bf4cf9c78d2aa5ffe41fd8965788334604
Viktor <vampirit@mail.ru>
2018-01-14 23:55:15 +0400

    not need

--- src/main/java/lunchVote/repository/RestaurantRepository.java
+++
@@ -1,7 +0,0 @@
-package lunchVote.repository;
-
-
-import lunchVote.model.Restaurant;
-
-public interface RestaurantRepository extends MyCrudRepository<Restaurant>{
-}
--- src/main/java/lunchVote/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package lunchVote.repository;
-
-import lunchVote.model.User;
-
-public interface UserRepository extends MyCrudRepository<User>{
-    User getByEmail(String email);
-}
