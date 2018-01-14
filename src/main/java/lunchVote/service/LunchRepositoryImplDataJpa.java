3f394024338b71fdfaa5b51c956a62f51b492416
Viktor <vampirit@mail.ru>
2018-01-15 00:05:28 +0400

    refactor logic

---
+++ src/main/java/lunchVote/service/LunchRepositoryImplDataJpa.java
@@ -0,0 +1,53 @@
+package lunchVote.service;
+
+import lunchVote.model.Lunch;
+import lunchVote.model.Restaurant;
+import lunchVote.repository.dataJpa.springCrud.LunchCrud;
+import lunchVote.repository.dataJpa.springCrud.RestaurantCrud;
+import lunchVote.transferObjects.LunchTransfer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class LunchRepositoryImplDataJpa implements LunchService{
+
+    private final LunchCrud crud;
+
+    private final RestaurantCrud restaurantCrud;
+
+    @Autowired
+    public LunchRepositoryImplDataJpa(LunchCrud crud, RestaurantCrud restaurantCrud) {
+        this.crud = crud;
+        this.restaurantCrud = restaurantCrud;
+    }
+
+    @Override
+    public List<Lunch> getAllForDate(LocalDate date) {
+        return crud.getByDate(date);
+    }
+
+    @Override
+    @Transactional
+    public Lunch save(LunchTransfer lunch) {
+        Restaurant one = restaurantCrud.getOne(lunch.getRestaurantId());
+        Integer price = new Double(lunch.getPrice() * 100).intValue();
+        Lunch toSave = new Lunch(lunch.getId(), lunch.getDate(), lunch.getDescription(), price, one);
+        return crud.save(toSave);
+    }
+
+    @Override
+    @Transactional
+    public boolean delete(int id) {
+        return crud.delete(id) != 0;
+    }
+
+    @Override
+    public Lunch get(int id) {
+        return crud.findById(id).orElse(null);
+    }
+}
