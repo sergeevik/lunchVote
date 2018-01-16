8be3550e3bf07bde69c9cb669df7cdcbbf450993
Viktor <vampirit@mail.ru>
2018-01-16 19:03:26 +0400

    change parent, need execute sql populate

--- src/test/java/lunchVote/service/cacheTest/CacheConfig.java
+++ src/test/java/lunchVote/service/cacheTest/CacheConfig.java
@@ -1,12 +1,12 @@
 package lunchVote.service.cacheTest;
 
-import lunchVote.SpringConfigOnTests;
+import lunchVote.repository.SQLAnnotation;
 import lunchVote.repository.queryCounter.CountInterceptor;
 import org.junit.Rule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 
-public abstract class CacheConfig extends SpringConfigOnTests{
+public abstract class CacheConfig extends SQLAnnotation {
 
     @Autowired
     protected CacheManager cache;
