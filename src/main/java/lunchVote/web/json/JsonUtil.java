edbbd78cc2fa39c987a7f7390b8f58365d97e389
Viktor <vampirit@mail.ru>
2018-01-17 16:24:33 +0400

    json

---
+++ src/main/java/lunchVote/web/json/JsonUtil.java
@@ -0,0 +1,60 @@
+package lunchVote.web.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectReader;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static lunchVote.web.json.JacksonObjectMapper.getMapper;
+
+public class JsonUtil {
+    public static <T> List<T> readValues(String json, Class<T> clazz) {
+        ObjectReader reader = getMapper().readerFor(clazz);
+        try {
+            return reader.<T>readValues(json).readAll();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read array from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> T readValue(String json, Class<T> clazz) {
+        try {
+            return getMapper().readValue(json, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> String writeValue(T obj) {
+        try {
+            return getMapper().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Invalid write to JSON:\n'" + obj + "'", e);
+        }
+    }
+
+    public static <T> String writeIgnoreProps(Collection<T> collection, String... ignoreProps) {
+        List<Map<String, Object>> list = collection.stream()
+                .map(e -> getAsMapWithIgnore(e, ignoreProps))
+                .collect(Collectors.toList());
+        return writeValue(list);
+    }
+
+    public static <T> String writeIgnoreProps(T obj, String... ignoreProps) {
+        Map<String, Object> map = getAsMapWithIgnore(obj, ignoreProps);
+        return writeValue(map);
+    }
+
+    private static <T> Map<String, Object> getAsMapWithIgnore(T obj, String[] ignoreProps) {
+        Map<String, Object> map = getMapper().convertValue(obj, new TypeReference<Map<String, Object>>() {});
+        for (String prop : ignoreProps) {
+            map.remove(prop);
+        }
+        return map;
+    }
+}
