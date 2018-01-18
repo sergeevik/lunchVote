d2b82aeed4ccc6196bc1b03675e56620ed975049
Viktor <vampirit@mail.ru>
2018-01-19 01:28:51 +0400

    restaurant valid realize

--- src/main/java/lunchVote/service/RestaurantServiceImpl.java
+++ src/main/java/lunchVote/service/RestaurantServiceImpl.java
@@ -2,6 +2,7 @@ package lunchVote.service;
 
 import lunchVote.model.Restaurant;
 import lunchVote.repository.dataJpa.springCrud.RestaurantCrud;
+import lunchVote.util.ValidateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -9,6 +10,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static lunchVote.util.ValidateUtil.checkEntityNotNull;
+import static lunchVote.util.ValidateUtil.checkIdEquals;
+import static lunchVote.util.ValidateUtil.checkNew;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,6 +30,7 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Transactional
     @CacheEvict(value = "restaurant", allEntries = true)
     public Restaurant create(Restaurant restaurant) {
+        checkNew(restaurant);
         return crud.save(restaurant);
     }
 
@@ -32,7 +38,10 @@ public class RestaurantServiceImpl implements RestaurantService {
     @Transactional
     @CacheEvict(value = "restaurant", allEntries = true)
     public Restaurant update(Restaurant restaurant, int restaurantId) {
-        return crud.save(restaurant);
+        checkIdEquals(restaurant, restaurantId);
+        Restaurant save = crud.save(restaurant);
+        checkEntityNotNull(save, restaurantId);
+        return save;
     }
 
     @Override
@@ -50,6 +59,8 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public Restaurant get(int id) {
-        return crud.findById(id).orElse(null);
+        Restaurant restaurant = crud.findById(id).orElse(null);
+        checkEntityNotNull(restaurant, id);
+        return restaurant;
     }
 }
