98945780a00fe67d0015f8941c5c89c970f81e06
Viktor <vampirit@mail.ru>
2018-01-18 05:09:13 +0400

    lunch rest realize

--- src/main/java/lunchVote/web/LunchRest.java
+++ src/main/java/lunchVote/web/LunchRest.java
@@ -2,15 +2,22 @@ package lunchVote.web;
 
 import lunchVote.model.Lunch;
 import lunchVote.service.LunchService;
+import lunchVote.transferObjects.LunchTransfer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
+
 @RestController
-@RequestMapping("/lunches")
+@RequestMapping(LunchRest.URL)
 public class LunchRest {
+    public static final String URL = "/lunches";
 
     private final LunchService lunchService;
 
@@ -19,20 +26,34 @@ public class LunchRest {
         this.lunchService = lunchService;
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Lunch create(Lunch lunch){
-        return null;
+    @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Lunch create(@RequestBody LunchTransfer lunch){
+        return lunchService.save(lunch);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@RequestBody Lunch lunch, @PathVariable("id") int id){
+    public void update(@RequestBody LunchTransfer lunch, @PathVariable("id") int id){
+        lunchService.save(lunch);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Lunch> getOnDay(@RequestParam("date") String date){
-        return null;
+    public List<Lunch> getOnDay(@RequestParam(value = "date", required = false) @DateTimeFormat(iso = DATE) LocalDate date){
+        if (date == null)
+            return lunchService.getAllForDate(LocalDate.now());
+        else {
+            return lunchService.getAllForDate(date);
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Lunch get(@PathVariable("id") int id){
+        return lunchService.get(id);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable("id") int id){}
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id){
+        lunchService.delete(id);
+    }
 }
