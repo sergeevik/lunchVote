69cc068da790f7375d3f989e1541db57694a332d
Viktor <vampirit@mail.ru>
2018-01-21 00:12:55 +0400

    tests work with DataBase, need refresh after each test

--- src/test/java/lunchVote/web/security/SecurityConfig.java
+++ src/test/java/lunchVote/web/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import lunchVote.web.MvcConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -13,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ContextConfiguration({"classpath:spring/spring-security.xml"})
+@Sql(scripts = "classpath:db/populateDb.sql", config = @SqlConfig(encoding = "UTF-8"))
 public abstract class SecurityConfig extends MvcConfig {
 
     @Autowired
